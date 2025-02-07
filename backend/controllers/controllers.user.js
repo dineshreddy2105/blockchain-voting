98c42fe87a602c60528ec9848056c51691753ca0
akhiranandha <kodamakhira624@gmail.com>
2025-02-07 17:30:28 +0530

    fixed backend

--- backend/controllers/controllers.user.js
+++ backend/controllers/controllers.user.js
@@ -3,7 +3,7 @@ const {userModel}=require("../models/models.user")
 async function getUserByAadhar(req,res){
     const {aadharid}=req.params
     try{
-        const user=await userModel.find({"aadharid":aadharid})
+        const user=await userModel.findOne({"aadharid":aadharid})
         res.status(200).json(user)
     }
     catch(err){
@@ -14,8 +14,9 @@ async function getUserByAadhar(req,res){
 async function getUserMailByAadhar(req,res) {
     const {aadharid}=req.params
     try{
-        const user=await userModel.find({"aadharid":aadharid})
-        res.status(200).json(user.aadharid)
+        const user=await userModel.findOne({"aadharid":aadharid})
+        console.log(user,"\n",user.email)
+        res.status(200).json(user.email)
     }
     catch(err){
         res.status(500).json({message:"Error in getting user details"})
