10bffaa6f3aafa260ba7401e22e43403935b4411
akhiranandha <kodamakhira624@gmail.com>
2025-02-10 15:58:30 +0530

    merged

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,11 @@ import AuthProvider from "./providers/AuthProvider";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UserLoginPage from "./components/UserComponents/UserLoginPage";
-<<<<<<< HEAD
 import AdminLoginPage from "./components/AdminComponents/AdminLoginPage";
 import UserSignUpPage from "./components/UserComponents/UserSignUpPage";
 import VoterProtectedRoute from "./middlewares/VoterProtectedRoutes"
 import AdminProtectedRoute from "./middlewares/AdminProtectedRoutes";
 import {ToastContainer} from "react-toastify";
-=======
-import AdminSignInPage from "./components/AdminComponents/AdminSignInPage";
-import UserSignInPage from "./components/UserComponents/UserSignInPage";
->>>>>>> 4caa0d3ac5f8bb69707ee8c62dcc23dd2201d44a
 function App() {
   //const [contractAddress, setContractAddress] = useState("");
 
@@ -24,7 +19,6 @@ function App() {
     <>
     <ToastContainer position="top-right" />
     <Router>
-<<<<<<< HEAD
       <AuthProvider>
         <div className="container-fluid">
           <Routes>
@@ -43,18 +37,6 @@ function App() {
           </Routes>
         </div>
       </AuthProvider>
-=======
-      <div className="container-fluid">
-        <Routes>
-          <Route path="/admin_panel" element={<AdminPanel />} />
-          <Route path="/user_panel" element={<UserPanel />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<UserLoginPage />} />
-          <Route path="/admin-signin" element={<AdminSignInPage />} />
-          <Route path="/sign-up" element={<UserSignInPage />} />
-        </Routes>
-      </div>
->>>>>>> 4caa0d3ac5f8bb69707ee8c62dcc23dd2201d44a
     </Router>
     </>
   );
